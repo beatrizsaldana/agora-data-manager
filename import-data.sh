3d85113119aaa7d6ace69e12f4113f77f73587dd
Khai Do <zaro0508@gmail.com>
2018-11-02 16:22:04 -0700

    mongoimport needs host and database params as well (#3)

--- import-data.sh
+++ import-data.sh
@@ -13,11 +13,11 @@ scp -i ./toptal_org-sagebase-scicomp.pem -r $DATA_DIR ec2-user@$BASTIAN_HOST:~/$
 
 # Imports the data and wipes the current collections from the bastian
 # Not using --mode upsert fow now because we don't have unique indexes properly set for the collections
-ssh -i ./toptal_org-sagebase-scicomp.pem ec2-user@$BASTIAN_HOST mongoimport --db db_host_$TRAVIS_BRANCH --collection genes --jsonArray --drop --file $REMOTE_DATA_DIR/rnaseq_differential_expression.json
-ssh -i ./toptal_org-sagebase-scicomp.pem ec2-user@$BASTIAN_HOST mongoimport --db db_host_$TRAVIS_BRANCH --collection geneslinks --jsonArray --drop --file $REMOTE_DATA_DIR/network.json
-ssh -i ./toptal_org-sagebase-scicomp.pem ec2-user@$BASTIAN_HOST mongoimport --db db_host_$TRAVIS_BRANCH --collection geneinfo --jsonArray --drop --file $REMOTE_DATA_DIR/gene_info.json
-ssh -i ./toptal_org-sagebase-scicomp.pem ec2-user@$BASTIAN_HOST mongoimport --db db_host_$TRAVIS_BRANCH --collection teaminfo --jsonArray --drop --file $REMOTE_DATA_DIR/team_info.json
+ssh -i ./toptal_org-sagebase-scicomp.pem ec2-user@$BASTIAN_HOST mongoimport -h DB_HOST_$TRAVIS_BRANCH -d agora -u DB_USER_$TRAVIS_BRANCH -p DB_PASS_$TRAVIS_BRANCH --collection genes --jsonArray --drop --file $REMOTE_DATA_DIR/rnaseq_differential_expression.json
+ssh -i ./toptal_org-sagebase-scicomp.pem ec2-user@$BASTIAN_HOST mongoimport -h DB_HOST_$TRAVIS_BRANCH -d agora -u DB_USER_$TRAVIS_BRANCH -p DB_PASS_$TRAVIS_BRANCH --collection geneslinks --jsonArray --drop --file $REMOTE_DATA_DIR/network.json
+ssh -i ./toptal_org-sagebase-scicomp.pem ec2-user@$BASTIAN_HOST mongoimport -h DB_HOST_$TRAVIS_BRANCH -d agora -u DB_USER_$TRAVIS_BRANCH -p DB_PASS_$TRAVIS_BRANCH --collection geneinfo --jsonArray --drop --file $REMOTE_DATA_DIR/gene_info.json
+ssh -i ./toptal_org-sagebase-scicomp.pem ec2-user@$BASTIAN_HOST mongoimport -h DB_HOST_$TRAVIS_BRANCH -d agora -u DB_USER_$TRAVIS_BRANCH -p DB_PASS_$TRAVIS_BRANCH --collection teaminfo --jsonArray --drop --file $REMOTE_DATA_DIR/team_info.json
 
 pushd $CURRENT_DIR/data/team_images
-ls -1r *.jpg | while read x; do ssh -i ./toptal_org-sagebase-scicomp.pem ec2-user@$BASTIAN_HOST mongofiles -d db_host_$TRAVIS_BRANCH -v put $x --replace; echo $x; done
+ls -1r *.jpg | while read x; do ssh -i ./toptal_org-sagebase-scicomp.pem ec2-user@$BASTIAN_HOST mongofiles -h DB_HOST_$TRAVIS_BRANCH -d agora -u DB_USER_$TRAVIS_BRANCH -p DB_PASS_$TRAVIS_BRANCH -v put $x --replace; echo $x; done
 popd
