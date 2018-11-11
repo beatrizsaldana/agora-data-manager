e724245bca2591d5e34372c38e98d6f7bccabcfd
Khai Do <zaro0508@gmail.com>
2018-11-11 08:39:46 -0800

    attempt 5

--- updatedb.sh
+++ updatedb.sh
@@ -15,4 +15,7 @@ ssh -i ~/.ssh/toptal_org-sagebase-scicomp.pem ec2-user@$BASTIAN_HOST "mkdir -p /
 
 scp -i ~/.ssh/toptal_org-sagebase-scicomp.pem import-data.sh ec2-user@$BASTIAN_HOST:/tmp/work/.
 
-ssh -i ~/.ssh/toptal_org-sagebase-scicomp.pem ec2-user@$BASTIAN_HOST "bash /tmp/work/import-data.sh $TRAVIS_BRANCH $SYNAPSE_USERNAME $SYNAPSE_PASSWORD $DB_HOST $DB_USER $DB_PASS"
+q_mid=\'\\\'\'
+SYNAPSE_PASSWORD_esc="'${SYNAPSE_PASSWORD//\'/$q_mid}'"
+
+ssh -i ~/.ssh/toptal_org-sagebase-scicomp.pem ec2-user@$BASTIAN_HOST "bash /tmp/work/import-data.sh $TRAVIS_BRANCH $SYNAPSE_USERNAME $SYNAPSE_PASSWORD_esc $DB_HOST $DB_USER $DB_PASS"
