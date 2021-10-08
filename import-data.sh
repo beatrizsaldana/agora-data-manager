cbad4860807af916a58c8cb8d0cf4c767943e84d
Jessica Britton <jessica.britton@sagebase.org>
2021-10-08 12:21:10 -0700

    download jpeg image files in addition to jpg image files

--- import-data.sh
+++ import-data.sh
@@ -52,5 +52,5 @@ mongoimport -h $DB_HOST -d agora -u $DB_USER -p $DB_PASS --authenticationDatabas
 mongoimport -h $DB_HOST -d agora -u $DB_USER -p $DB_PASS --authenticationDatabase admin --collection genesoverallscores --jsonArray --drop --file $DATA_DIR/overall_scores.json
 
 pushd $TEAM_IMAGES_DIR
-ls -1r *.jpg | while read x; do mongofiles -h $DB_HOST -d agora -u $DB_USER -p $DB_PASS --authenticationDatabase $DB_USER -v put $x --replace; echo $x; done
+ls -1r *.{jpg,jpeg} | while read x; do mongofiles -h $DB_HOST -d agora -u $DB_USER -p $DB_PASS --authenticationDatabase $DB_USER -v put $x --replace; echo $x; done
 popd
