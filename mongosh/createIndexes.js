99f3cfa1397ad265b01b9a64fdb440407a20713d
Matthias Courville <matthias@genui.com>
2022-02-14 11:49:22 -0800

    Index creation test

---
+++ mongosh/createIndexes.js
@@ -0,0 +1,28 @@
+db = connect( 'agora' );
+
+print('Database: ' + db.getName());
+
+db.teaminfo.createIndex({
+    'program'   : 1,
+    'team_full' : 1
+});
+
+var teamInfoIndexes = db.teaminfo.getIndexes();
+var teamInfoSortIndex;
+
+if (teamInfoIndexes) {
+    for (let index of teamInfoIndexes) {
+        if ('program_1_team_full_1' == index.name) {
+            teamInfoSortIndex = index;
+            break;
+        }
+    }
+}
+
+if (teamInfoSortIndex) {
+    print('Index added for teaminfo:');
+    printjson(teamInfoSortIndex);
+}
+else {
+    print('Index creation failed for teaminfo...');
+}
