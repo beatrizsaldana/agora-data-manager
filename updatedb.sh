6c9ebf7c209ad9b0d7acdf772169d42fbc3a17b9
Khai Do <3697686+zaro0508@users.noreply.github.com>
2022-08-22 15:35:42 -0700

    [IT-1914] Update for new agora env (#65)
    This change allows the travis CI to deploy DB updates to the new agora
    environment.  For isolation purposes the new environment has a bastian
    for each environment instead of one bastian servicing all environments.

--- updatedb.sh
+++ updatedb.sh
@@ -4,6 +4,7 @@
 set -e
 
 # double interpolate vars from travis
+eval export "BASTIAN_HOST=\$BASTIAN_HOST_$TRAVIS_BRANCH"
 eval export "DB_HOST=\$DB_HOST_$TRAVIS_BRANCH"
 eval export "DB_USER=\$DB_USER_$TRAVIS_BRANCH"
 eval export "DB_PASS=\$DB_PASS_$TRAVIS_BRANCH"
@@ -15,14 +16,21 @@ SYNAPSE_PASSWORD_ESC="'${SYNAPSE_PASSWORD//\'/$q_mid}'"
 DB_USER_ESC="'${DB_USER//\'/$q_mid}'"
 DB_PASS_ESC="'${DB_PASS//\'/$q_mid}'"
 
+# bastian configs
+BASTIAN_USER="ec2-user"
+BASTIAN_SSH_KEY="~/.ssh/agora-ci-$TRAVIS_BRANCH.pem"
+
+# set SSH configuration
+echo -e "Host $BASTIAN_HOST\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+
 # clean up from previous builds
-ssh -i ~/.ssh/agora-travis.pem ec2-user@$BASTIAN_HOST "rm -rf /tmp/work"
+ssh -i $BASTIAN_SSH_KEY $BASTIAN_USER@$BASTIAN_HOST "rm -rf /tmp/work"
 
 # create directories for data
-ssh -i ~/.ssh/agora-travis.pem ec2-user@$BASTIAN_HOST "mkdir -p /tmp/work/data/team_images"
+ssh -i $BASTIAN_SSH_KEY $BASTIAN_USER@$BASTIAN_HOST "mkdir -p /tmp/work/data/team_images"
 
 # setup script on bastian
-scp -i ~/.ssh/agora-travis.pem import-data.sh data-manifest.json create-indexes.js ec2-user@$BASTIAN_HOST:/tmp/work/.
+scp -i $BASTIAN_SSH_KEY import-data.sh data-manifest.json create-indexes.js $BASTIAN_USER@$BASTIAN_HOST:/tmp/work/.
 
 # run import on bastian
-ssh -i ~/.ssh/agora-travis.pem ec2-user@$BASTIAN_HOST "bash /tmp/work/import-data.sh $TRAVIS_BRANCH $SYNAPSE_USERNAME_ESC $SYNAPSE_PASSWORD_ESC $DB_HOST $DB_USER_ESC $DB_PASS_ESC"
+ssh -i $BASTIAN_SSH_KEY $BASTIAN_USER@$BASTIAN_HOST "bash /tmp/work/import-data.sh $TRAVIS_BRANCH $SYNAPSE_USERNAME_ESC $SYNAPSE_PASSWORD_ESC $DB_HOST $DB_USER_ESC $DB_PASS_ESC"
